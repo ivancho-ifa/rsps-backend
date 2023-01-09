4cabe6f4909e7b746264fe812c788d9518c6ac1c
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-01-09 03:41:24 +0200

    Basic proceesing of tracker data
    This change separates the communication between the tracker and the
    server and the server and the frontend.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import mqtt from 'mqtt'
 
 
-const mqtt_client = mqtt.connect({
+const frontendClient = mqtt.connect({
 	protocol: 'ws',
 	hostname: 'broker.hivemq.com',
 	port: 8000,
 	path: '/mqtt'
 })
 
-mqtt_client.on('message', function (_topic, message) {
-	const message_parsed = message.toString()
-	console.log(`message: ${message_parsed}`)
+
+const trackerClient = mqtt.connect({
+	protocol: 'mqtt',
+	hostname: 'test.mosquitto.org',
+	port: 1883,
+})
+
+trackerClient.subscribe('rsps/trackers/+')
+
+trackerClient.on('message', function (topic, message) {
+	const messageParsed = message.toString()
+	console.log(`message: ${messageParsed}`)
+
+	frontendClient.publish(topic, messageParsed)
 })
