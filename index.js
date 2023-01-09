3036ef169fedf7028610d7e686fbd2f1b33315e4
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-01-09 03:17:13 +0200

    Separate backend and frontend
    After the separation of backend and frontend the backend remains just
    logging the incoming data from the tracker. Functionalities of the
    backend needs to be expanded and we can also probably change the
    language if it seems fit.

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const mqtt = require('mqtt')
-const path = require('path')
+import mqtt from 'mqtt'
 
 
 const mqtt_client = mqtt.connect({
-	port: 1883,
-	host: 'test.mosquitto.org',
-	protocol: 'mqtt'
+	protocol: 'ws',
+	hostname: 'broker.hivemq.com',
+	port: 8000,
+	path: '/mqtt'
 })
-
-const PORT = process.env.PORT || 3333
-
-let db = {
-	latitude: 0,
-	longitude: 0,
-	altitude: 0,
-	speed: 0,
-	locked: false
-}
-
 
 mqtt_client.on('message', function (_topic, message) {
 	const message_parsed = message.toString()
 	console.log(`message: ${message_parsed}`)
-
-	db = JSON.parse(message_parsed)
-	console.log('database:', db)
 })
-
-
-const app = express()
-const server = app
-	.use(bodyParser.json())                         // for parsing application/json
-	.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'jsx')
-	.engine('jsx', require('express-react-views').createEngine())
-	.get('/', function (request, response) {
-		response.render('Index')
-	})
-	.get('/tracker', function (request, response) {
-		mqtt_client.subscribe(`rsps/trackers/rsps-tracker-${request.query['tracker-id']}`)
-		setTimeout(function () {
-			response.render('TrackerInfo', db)
-		}, 3000)
-	})
-	.listen(PORT, function () {
-		app.locals.address = server.address().address
-		app.locals.port = server.address().port
-		console.log(`Listening on ${app.locals.address}:${app.locals.port}`)
-	})
